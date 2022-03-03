e0730df1fec161cb75d3f42614cd68da1143634b
lyl <272592782@qq.com>
2022-03-03 14:54:48 +0800

    管理后台商品控制器封装

--- app/Admin/Controllers/CrowdfundingProductsController.php
+++ app/Admin/Controllers/CrowdfundingProductsController.php
@@ -5,12 +5,12 @@ namespace App\Admin\Controllers;
 use App\Models\Category;
 use App\Models\CrowdfundingProduct;
 use App\Models\Product;
-use Encore\Admin\Controllers\AdminController;
+
 use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
 
-class CrowdfundingProductsController extends AdminController
+class CrowdfundingProductsController extends CommonProductsController
 {
     /**
      * Title for current resource.
@@ -19,11 +19,40 @@ class CrowdfundingProductsController extends AdminController
      */
     protected $title = '众筹商品';
 
+    public function getProductType()
+    {
+        return Product::TYPE_CROWDFUNDING;
+    }
+
+    protected function customGrid(Grid $grid)
+    {
+        $grid->id('ID')->sortable();
+        $grid->title('商品名称');
+        $grid->on_sale('已上架')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->price('价格');
+        $grid->column('crowdfunding.target_amount', '目标金额');
+        $grid->column('crowdfunding.end_at', '结束时间');
+        $grid->column('crowdfunding.total_amount', '目前金额');
+        $grid->column('crowdfunding.status', ' 状态')->display(function ($value) {
+            return CrowdfundingProduct::$statusMap[$value];
+        });
+    }
+
+    protected function customForm(Form $form)
+    {
+        // 众筹相关字段
+        $form->text('crowdfunding.target_amount', '众筹目标金额')->rules('required|numeric|min:0.01');
+        $form->datetime('crowdfunding.end_at', '众筹结束时间')->rules('required|date');
+    }
+
     /**
      * Make a grid builder.
      *
      * @return Grid
      */
+    /*
     protected function grid()
     {
 //        $grid = new Grid(new Product());
@@ -75,6 +104,7 @@ class CrowdfundingProductsController extends AdminController
 
         return $grid;
     }
+    */
 
     /**
      * Make a show builder.
@@ -108,6 +138,7 @@ class CrowdfundingProductsController extends AdminController
      *
      * @return Form
      */
+    /*
     protected function form()
     {
         $form = new Form(new Product);
@@ -134,7 +165,9 @@ class CrowdfundingProductsController extends AdminController
         });
 
         $form->saving(function (Form $form) {
-            $form->model()->price = collect($form->input('skus'))->where(Form::REMOVE_FLAG_NAME, 0)->min('price');
+            $form->model()->price = collect($form->input('skus'))
+                ->where(Form::REMOVE_FLAG_NAME, 0)
+                ->min('price');
         });
         return $form;
 
@@ -155,4 +188,5 @@ class CrowdfundingProductsController extends AdminController
 //
 //        return $form;
     }
+    */
 }
--- app/Admin/Controllers/ProductsController.php
+++ app/Admin/Controllers/ProductsController.php
@@ -9,7 +9,7 @@ use Encore\Admin\Form;
 use Encore\Admin\Grid;
 use Encore\Admin\Show;
 
-class ProductsController extends AdminController
+class ProductsController extends CommonProductsController
 {
     /**
      * Title for current resource.
@@ -18,11 +18,38 @@ class ProductsController extends AdminController
      */
     protected $title = '商品';
 
+    public function getProductType()
+    {
+        return Product::TYPE_NORMAL;
+    }
+
+
+    protected function customGrid(Grid $grid)
+    {
+        $grid->model()->with(['category']);
+        $grid->id('ID')->sortable();
+        $grid->title('商品名称');
+        $grid->column('category.name', '类目');
+        $grid->on_sale('已上架')->display(function ($value) {
+            return $value ? '是' : '否';
+        });
+        $grid->price('价格');
+        $grid->rating('评分');
+        $grid->sold_count('销量');
+        $grid->review_count('评论数');
+    }
+
+    protected function customForm(Form $form)
+    {
+        // 普通商品没有额外的字段，因此这里不需要写任何代码
+    }
+
     /**
      * Make a grid builder.
      *
      * @return Grid
      */
+    /*
     protected function grid()
     {
         $grid = new Grid(new Product);
@@ -58,12 +85,14 @@ class ProductsController extends AdminController
 
         return $grid;
     }
+*/
 
     /**
      * Make a form builder.
      *
      * @return Form
      */
+    /*
     protected function form()
     {
         $form = new Form(new Product);
@@ -106,4 +135,5 @@ class ProductsController extends AdminController
 
         return $form;
     }
+    */
 }
